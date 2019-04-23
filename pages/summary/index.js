c04a4943ff681e5dadf81eae69cf776ac44a80ab
胡佳婷 <hujt@chinatelecom.cn>
2019-04-23 22:09:06 +0800

    modal

--- pages/summary/index.js
+++ pages/summary/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {inject, observer} from 'mobx-react';
+import { inject, observer } from 'mobx-react';
 import Link from 'next/link';
 import {
     NavBar, List, InputItem, WhiteSpace, WingBlank, Button, Toast, Flex, Radio, Result, Icon,
     TextareaItem, Calendar, Card, Modal
 } from 'antd-mobile';
-import {createForm} from 'rc-form';
+import { createForm } from 'rc-form';
 import PageWrapper from '@components/PageWrapper';
 import RedPacketType from '@constants/RedPacketType';
 import agent from "@utils/agent";
@@ -13,7 +13,7 @@ import styles from './index.less';
 import pageStore from './store';
 import storage from '@utils/storage';
 import Router from 'next/router'
-import {Radar, Doughnut} from 'react-chartjs';
+import { Radar, Doughnut } from 'react-chartjs';
 
 const RadioItem = Radio.RadioItem;
 
@@ -23,7 +23,7 @@ class Page extends React.Component {
     constructor(props) {
         super(props);
 
-        const {query} = this.props;
+        const { query } = this.props;
         if (query.voteId) {
             this.voteId = query.voteId;
         }
@@ -33,10 +33,10 @@ class Page extends React.Component {
             doughnutData: null,
             radarData: null,
             posterVisible: false,
-			shareVisible: false,
+            shareVisible: false,
             testVisiable: false,
             bak: false,
-			attrId:0
+            attrId: 0
         };
     }
 
@@ -93,32 +93,32 @@ class Page extends React.Component {
 
         }
 
-	}
+    }
 
-	showModal = attrId =>{
-		const {testVisiable} = this.state;
-		this.setState({
-			testVisiable:!testVisiable,
-			attrId
-		})
-	}
-	handleModalTest = ()=>{
-		const {attrId} = this.state;
-		Router.push(`/report?attrId=${attrId}`);
-	}
+    showModal = attrId => {
+        const { testVisiable } = this.state;
+        this.setState({
+            testVisiable: !testVisiable,
+            attrId
+        })
+    }
+    handleModalTest = () => {
+        const { attrId } = this.state;
+        Router.push(`/report?attrId=${attrId}`);
+    }
 
     handleTest = (attr) => {
-		console.log(attr);
-		this.showModal(attr.attrId);
+        console.log(attr);
+        this.showModal(attr.attrId);
         // Router.push(`/report?attrId=${attr.attrId}`);
     };
 
     handleOpenPoster = () => {
-        this.setState({posterVisible: true});
+        this.setState({ posterVisible: true });
     };
 
     render() {
-        const {report = {}, doughnutData, radarData} = this.state;
+        const { report = {}, doughnutData, radarData } = this.state;
         const attrList = report.attrList || [];
         // const chartData = {
         //     labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
@@ -142,14 +142,14 @@ class Page extends React.Component {
                         <div className={styles.percent}>{report.percent || ''}</div>
                         {doughnutData && (
                             <Doughnut data={doughnutData}
-                                      width={100}
-                                      height={100}
-                                      options={{
-                                          segmentShowStroke: false,
-                                          percentageInnerCutout: 90,
-                                          animateRotate: false,
-                                          legendTemplate: 'test'
-                                      }}/>
+                                width={100}
+                                height={100}
+                                options={{
+                                    segmentShowStroke: false,
+                                    percentageInnerCutout: 90,
+                                    animateRotate: false,
+                                    legendTemplate: 'test'
+                                }} />
                         )}
                     </div>
                     <div className={styles.sentences}>
@@ -160,69 +160,69 @@ class Page extends React.Component {
                         })}
                     </div>
                     {/*<img src='/static/img/btn_poster.png' onClick={this.handleOpenPoster.bind(this)}*/}
-                         {/*style={{marginTop: '20px', width: '280px', height: '64px'}}/>*/}
+                    {/*style={{marginTop: '20px', width: '280px', height: '64px'}}/>*/}
                 </div>
                 <div className={styles.sectionBody}>
                     <div className={styles.radarWrapper}>
                         {radarData && (
                             <Radar data={radarData}
-                                   width={300}
-                                   height={300}
-                                   options={{
-                                       legend: {
-                                           display: false
-                                       }
-                                   }}/>
+                                width={300}
+                                height={300}
+                                options={{
+                                    legend: {
+                                        display: false
+                                    }
+                                }} />
                         )}
                     </div>
                     <div className={styles.summary}>
                         <div className={styles.summaryLabel}>智能总评</div>
                         <div className={styles.summaryContent}>{report.summary || ''}</div>
                         <img src='/static/img/btn_share.png' onClick={() => {
-                            this.setState({shareVisible: true})
-                        }} style={{marginTop: '20px', width: '165px', height: '24px'}}/>
+                            this.setState({ shareVisible: true })
+                        }} style={{ marginTop: '20px', width: '165px', height: '24px' }} />
                     </div>
                 </div>
                 <div className={styles.sectionMid}>
-                    <div style={{fontSize: '20px', textAlign: 'center'}}>优势智能</div>
+                    <div style={{ fontSize: '20px', textAlign: 'center' }}>优势智能</div>
                     <div className={styles.tips}>* 劣势智能如何增强请查看专业版报告</div>
                     <div className={styles.row}>
                         {attrList[0] && (
                             <div key={`attr-0`}
-                                 onClick={this.handleTest.bind(this, attrList[0])}
-                                 className={styles.cell}>
+                                onClick={this.handleTest.bind(this, attrList[0])}
+                                className={styles.cell}>
                                 <div className={styles.icon} style={{
                                     backgroundImage: 'url("/static/img/icon/icon_gold.png")'
                                 }}></div>
                                 <div className={styles.attr}>{attrList[0].attrName}</div>
                                 <div className={styles.bg}
-                                     style={{backgroundImage: 'url("/static/img/icon/icon_' + attrList[0].attrId + '.png")'}}
+                                    style={{ backgroundImage: 'url("/static/img/icon/icon_' + attrList[0].attrId + '.png")' }}
                                 ></div>
                                 <div className={styles.bottom} style={{
                                     background: '#fd8f37'
                                 }}>
                                     <div>优化建议 ></div>
-                                    <img className={styles.lock} src={'/static/img/icon/icon_lock01.png'}/>
+                                    <img className={styles.lock} src={'/static/img/icon/icon_lock01.png'} />
                                 </div>
                             </div>
                         )}
-                        <div style={{width: '20px'}}></div>
+                        <div style={{ width: '20px' }}></div>
                         {attrList[1] && (
                             <div key={`attr-1`}
-                                 onClick={this.handleTest.bind(this, attrList[1])}
-                                 className={styles.cell}>
+                                onClick={this.handleTest.bind(this, attrList[1])}
+                                className={styles.cell}>
                                 <div className={styles.icon} style={{
                                     backgroundImage: 'url("/static/img/icon/icon_silver.png")'
                                 }}></div>
                                 <div className={styles.attr}>{attrList[1].attrName}</div>
                                 <div className={styles.bg}
-                                     style={{backgroundImage: 'url("/static/img/icon/icon_' + attrList[1].attrId + '.png")'}}
+                                    style={{ backgroundImage: 'url("/static/img/icon/icon_' + attrList[1].attrId + '.png")' }}
                                 ></div>
                                 <div className={styles.bottom} style={{
                                     background: '#fd7d8f'
                                 }}>
                                     <div>优化建议 ></div>
-                                    <img className={styles.lock} src={'/static/img/icon/icon_lock01.png'}/>
+                                    <img className={styles.lock} src={'/static/img/icon/icon_lock01.png'} />
                                 </div>
                             </div>
                         )}
@@ -230,45 +230,45 @@ class Page extends React.Component {
                     <div className={styles.row}>
                         {attrList[2] && (
                             <div key={`attr-2`}
-                                 onClick={this.handleTest.bind(this, attrList[2])}
-                                 className={styles.cell}>
+                                onClick={this.handleTest.bind(this, attrList[2])}
+                                className={styles.cell}>
                                 <div className={styles.icon} style={{
                                     backgroundImage: 'url("/static/img/icon/icon_copper.png")'
                                 }}></div>
                                 <div className={styles.attr}>{attrList[2].attrName}</div>
                                 <div className={styles.bg}
-                                     style={{backgroundImage: 'url("/static/img/icon/icon_' + attrList[2].attrId + '.png")'}}
+                                    style={{ backgroundImage: 'url("/static/img/icon/icon_' + attrList[2].attrId + '.png")' }}
                                 ></div>
                                 <div className={styles.bottom} style={{
                                     background: '#afe568'
                                 }}>
                                     <div>优化建议 ></div>
-                                    <img className={styles.lock} src={'/static/img/icon/icon_lock01.png'}/>
+                                    <img className={styles.lock} src={'/static/img/icon/icon_lock01.png'} />
                                 </div>
                             </div>
                         )}
-                        <div style={{width: '20px'}}></div>
+                        <div style={{ width: '20px' }}></div>
                         {attrList[3] && (
                             <div key={`attr-3`}
-                                 onClick={this.handleTest.bind(this, attrList[3])}
-                                 className={styles.cell}>
+                                onClick={this.handleTest.bind(this, attrList[3])}
+                                className={styles.cell}>
                                 <div className={styles.attr}>{attrList[3].attrName}</div>
                                 <div className={styles.bg}
-                                     style={{backgroundImage: 'url("/static/img/icon/icon_' + attrList[3].attrId + '.png")'}}
+                                    style={{ backgroundImage: 'url("/static/img/icon/icon_' + attrList[3].attrId + '.png")' }}
                                 ></div>
                                 <div className={styles.bottom} style={{
                                     background: '#5fd8ef'
                                 }}>
                                     <div>优化建议 ></div>
-                                    <img className={styles.lock} src={'/static/img/icon/icon_lock01.png'}/>
+                                    <img className={styles.lock} src={'/static/img/icon/icon_lock01.png'} />
                                 </div>
                             </div>
                         )}
                     </div>
                 </div>
                 <div className={styles.sectionBottom}>
-                    <img style={{width: '100%'}} src={'/static/img/result_work.png'} onClick={() => {this.setState({bak: true})}}/>
-                    <img style={{width: '100%'}} src={'/static/img/result_pro.png'} onClick={() => {this.setState({bak: true})}}/>
+                    <img style={{ width: '100%' }} src={'/static/img/result_work.png'} onClick={() => { this.setState({ bak: true }) }} />
+                    <img style={{ width: '100%' }} src={'/static/img/result_pro.png'} onClick={() => { this.setState({ bak: true }) }} />
                 </div>
                 {/*<img className={styles.page} src='/static/img/page/result_01.png'/>*/}
                 {/*<img onClick={this.handleTest} className={styles.page} src='/static/img/page/result_02.png'/>*/}
@@ -283,44 +283,50 @@ class Page extends React.Component {
                         background: 'rgba(0, 0, 0, .7)'
                     }}
 
-                         onClick={() => {
-                             this.setState({shareVisible: false})
-                         }}>
-                        <img style={{width: '320px', height: '320px', position: 'fixed', top: 0, right: 0}}
-                             src={'/static/img/sharemask.png'}/>
+                        onClick={() => {
+                            this.setState({ shareVisible: false })
+                        }}>
+                        <img style={{ width: '320px', height: '320px', position: 'fixed', top: 0, right: 0 }}
+                            src={'/static/img/sharemask.png'} />
                     </div>
                 )}
                 <Modal visible={this.state.posterVisible}
-                       transparent={true}
-                       maskClosable={true}
-                       platform={'android'}
-                       onClose={() => {
-                           this.setState({posterVisible: false})
-                       }}
+                    transparent={true}
+                    maskClosable={true}
+                    platform={'android'}
+                    onClose={() => {
+                        this.setState({ posterVisible: false })
+                    }}
                 >
-                    <img style={{width: '100%'}} src={'/static/img/poster01.png'}/>
+                    <img style={{ width: '100%' }} src={'/static/img/poster01.png'} />
                 </Modal>
                 <Modal visible={this.state.testVisiable}
-                       transparent={true}
+                    transparent={true}
                     //    maskClosable={true}
                     //    platform={'android'}
-                       closable={true}
-                       onClose={() => {
-                           this.setState({testVisiable: false})
-                       }}
+                    closable={true}
+                    onClose={() => {
+                        this.setState({ testVisiable: false })
+                    }}
                 >
                     <div>
-						<div>请输入邀请码</div>
-						<InputItem className={styles.item__name}></InputItem>
-						<div onClick={this.handleModalTest}>提交</div>
-					</div>
+                        <div className={styles['modal-title-wrapper']}>
+                            <div className={styles['modal-title']}>请输入邀请码解锁报告</div>
+                            <div className={styles['modal-title']}>或支付<span className={styles.emphasize}>￥1.99</span>查看</div>
+                        </div>
+                        <InputItem className={styles.item__name} placeholder="请输入邀请码"></InputItem>
+                        <div className={styles.inline}>
+                            <Button onClick={this.handleModalTest}>确定</Button>
+                            <Button onClick={this.handleModalTest} type="primary">支付</Button>
+                        </div>
+                    </div>
                 </Modal>
-				<Modal visible={this.state.bak}
-                       transparent={true}
-                       closable={true}
-                       onClose={() => {
-                           this.setState({bak: false})
-                       }}
+                <Modal visible={this.state.bak}
+                    transparent={true}
+                    closable={true}
+                    onClose={() => {
+                        this.setState({ bak: false })
+                    }}
                 >
                     <p>即将上线，敬请期待！</p>
                 </Modal>
