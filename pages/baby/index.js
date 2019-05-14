1d8716b2394bc9feaed27d9c830ff17d38ca3836
胡佳婷 <hujt@chinatelecom.cn>
2019-05-14 16:48:36 +0800

    styles

--- pages/baby/index.js
+++ pages/baby/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import {
-  inject,
-  observer
-} from 'mobx-react';
+import { inject, observer } from 'mobx-react';
 import Link from 'next/link';
 import {
   NavBar,
@@ -23,9 +20,7 @@ import {
   DatePicker,
   Modal
 } from 'antd-mobile';
-import {
-  createForm
-} from 'rc-form';
+import { createForm } from 'rc-form';
 import moment from 'moment';
 import classNames from 'classnames';
 import PageWrapper from '@components/PageWrapper';
@@ -42,9 +37,7 @@ class Page extends React.Component {
   constructor(props) {
     super(props);
 
-    const {
-      query
-    } = this.props;
+    const { query } = this.props;
     if (query.voteId) {
       this.voteId = query.voteId;
     }
@@ -79,7 +72,9 @@ class Page extends React.Component {
           hasReport: true
         });
       }
-    } catch (e) {} finally {}
+    } catch (e) {
+    } finally {
+    }
   }
 
   onPhoneErrorClick = () => {
@@ -104,8 +99,8 @@ class Page extends React.Component {
   changeBirthday = value => {
     if (
       moment(value)
-      .add(6, 'y')
-      .isAfter(new Date())
+        .add(6, 'y')
+        .isAfter(new Date())
     ) {
       this.setState({
         birthday: value
@@ -117,14 +112,7 @@ class Page extends React.Component {
 
   handleSubmit = async () => {
     try {
-      const {
-        hasReport,
-        hasInfo,
-        birthday,
-        name,
-        mobile,
-        gender
-      } = this.state;
+      const { hasReport, hasInfo, birthday, name, mobile, gender } = this.state;
       if (hasReport) {
         Router.push('/summary');
       } else {
@@ -144,7 +132,9 @@ class Page extends React.Component {
         }
         Router.push('/question');
       }
-    } catch (e) {} finally {}
+    } catch (e) {
+    } finally {
+    }
   };
   // validateDatePicker = (rule, date, callback) => {
   //     if (moment(date).add(1, 'y').isAfter(new Date())) {
@@ -169,216 +159,135 @@ class Page extends React.Component {
       phoneError,
       mobile
     } = this.state;
-    const CustomChildren = ({
-      extra,
-      onClick,
-      children
-    }) => ( <
-      div onClick = {
-        onClick
-      }
-      className = {
-        styles.text
-      }
-      style = {
-        {
+    const CustomChildren = ({ extra, onClick, children }) => (
+      <div
+        onClick={onClick}
+        className={styles.text}
+        style={{
           backgroundColor: '#fff',
           height: '40px',
           lineHeight: '40px',
           color: hasInfo ? '#bbb' : '#507DDC'
-        }
-      } >
-      {
-        extra
-      } <
-      /div>
+        }}
+      >
+        {extra}{' '}
+      </div>
     );
-    return ( <
-      div className = {
-        styles.page
-      } >
-      <
-      div >
-      <
-      img className = {
-        styles.head
-      }
-      src = "/static/img/baby/head.png" / >
-      <
-      /div> <
-      div className = {
-        styles.card
-      } >
-      <
-      div className = {
-        styles.card__body
-      } >
-      <
-      div className = {
-        styles.item
-      } >
-      <
-      div className = {
-        styles.label
-      } > 宝宝生日 < /div> <
-      DatePicker
-      // {...getFieldProps('birthday', {
-      //     initialValue: new Date(),
-      //     rules: [
-      //         { required: true, message: '请选择出生日期' },
-      //         { validator: this.validateDatePicker },
-      //     ],
-      // })}
-      mode = "date"
-      format = "YYYY-MM-DD"
-      title = "请选择日期"
-      value = {
-        birthday
-      }
-      onChange = {
-        this.changeBirthday
-      }
-      extra = "请点击选择"
-      disabled = {
-        hasInfo
-      } >
-      <
-      CustomChildren / >
-      <
-      /DatePicker> <
-      /div> <
-      div className = {
-        styles.item
-      } >
-      <
-      div className = {
-        styles.label
-      } > 宝宝性别 < /div> <
-      div className = {
-        styles.inline
-      } >
-      <
-      div className = {
-        styles['gender-wrapper']
-      }
-      onClick = {
-        () => this.chooseGender(1)
-      } >
-      <
-      img className = {
-        styles.item__gender
-      }
-      src = "/static/img/icon/boy.png" /
-      >
-      <
-      div className = {
-        styles.text
-      }
-      style = {
-        gender == 1 ? {
-          color: '#3366cc'
-        } : {
-          color: '#d9d9d9'
-        }
-      } >
-      男 <
-      /div> {
-        /* <img className={styles.item__checked} style={gender == 1 ? { display: 'block' } : { display: 'none' }} src="/static/img/icon/selected.png" /> */ } <
-      /div> <
-      div className = {
-        styles['gender-wrapper']
-      }
-      onClick = {
-        () => this.chooseGender(2)
-      } >
-      <
-      img className = {
-        styles.item__gender
-      }
-      src = "/static/img/icon/girl.png" /
-      >
-      <
-      div className = {
-        styles.text
-      }
-      style = {
-        gender == 2 ? {
-          color: '#3366cc'
-        } : {
-          color: '#d9d9d9'
-        }
-      } >
-      女 <
-      /div> {
-        /* <img className={styles.item__checked} style={gender == 2 ? { display: 'block' } : { display: 'none' }} src="/static/img/icon/selected.png" /> */ } <
-      /div> <
-      /div> <
-      /div> <
-      div className = {
-        styles.item
-      } >
-      <
-      div className = {
-        styles.label
-      } > 宝宝昵称 < /div> <
-      InputItem className = {
-        styles.item__name
-      }
-      placeholder = ""
-      value = {
-        this.state.name
-      }
-      onChange = {
-        v => this.setState({
-          name: v
-        })
-      }
-      disabled = {
-        hasInfo
-      }
-      /> <
-      /div> <
-      div className = {
-        styles.item
-      } >
-      <
-      div className = {
-        styles.label
-      } > 手机号 < /div> <
-      InputItem type = "phone"
-      className = {
-        styles.item__name
-      }
-      placeholder = ""
-      value = {
-        mobile
-      }
-      error = {
-        phoneError
-      }
-      onErrorClick = {
-        this.onPhoneErrorClick
-      }
-      onChange = {
-        this.changePhone
-      }
-      disabled = {
-        hasInfo
-      }
-      /> <
-      /div> <
-      div className = {
-        styles.submit
-      }
-      onClick = {
-        this.handleSubmit
-      } > {
-        hasReport ? '查看报告' : '开始测试'
-      } <
-      /div> <
-      /div> <
-      /div> <
-      /div>
+    return (
+      <div className={styles.page}>
+        <div>
+          <img className={styles.head} src="/static/img/baby/head.png" />
+        </div>{' '}
+        <div className={styles.card}>
+          <div className={styles.card__body}>
+            <div className={styles.item}>
+              <div className={styles.label}> 宝宝生日 </div>{' '}
+              <DatePicker
+                // {...getFieldProps('birthday', {
+                //     initialValue: new Date(),
+                //     rules: [
+                //         { required: true, message: '请选择出生日期' },
+                //         { validator: this.validateDatePicker },
+                //     ],
+                // })}
+                mode="date"
+                format="YYYY-MM-DD"
+                title="请选择日期"
+                value={birthday}
+                onChange={this.changeBirthday}
+                extra="请点击选择"
+                disabled={hasInfo}
+              >
+                <CustomChildren />
+              </DatePicker>{' '}
+            </div>{' '}
+            <div className={styles.item}>
+              <div className={styles.label}> 宝宝性别 </div>{' '}
+              <div className={styles.inline}>
+                <div
+                  className={styles['gender-wrapper']}
+                  onClick={() => this.chooseGender(1)}
+                >
+                  <img
+                    className={styles.item__gender}
+                    src="/static/img/icon/boy.png"
+                  />
+                  <div
+                    className={styles.text}
+                    style={
+                      gender == 1
+                        ? {
+                            color: '#3366cc'
+                          }
+                        : {
+                            color: '#d9d9d9'
+                          }
+                    }
+                  >
+                    男{' '}
+                  </div>{' '}
+                  {/* <img className={styles.item__checked} style={gender == 1 ? { display: 'block' } : { display: 'none' }} src="/static/img/icon/selected.png" /> */}{' '}
+                </div>{' '}
+                <div
+                  className={styles['gender-wrapper']}
+                  onClick={() => this.chooseGender(2)}
+                >
+                  <img
+                    className={styles.item__gender}
+                    src="/static/img/icon/girl.png"
+                  />
+                  <div
+                    className={styles.text}
+                    style={
+                      gender == 2
+                        ? {
+                            color: '#3366cc'
+                          }
+                        : {
+                            color: '#d9d9d9'
+                          }
+                    }
+                  >
+                    女{' '}
+                  </div>{' '}
+                  {/* <img className={styles.item__checked} style={gender == 2 ? { display: 'block' } : { display: 'none' }} src="/static/img/icon/selected.png" /> */}{' '}
+                </div>{' '}
+              </div>{' '}
+            </div>{' '}
+            <div className={styles.item}>
+              <div className={styles.label}> 宝宝昵称 </div>{' '}
+              <InputItem
+                className={styles.item__name}
+                placeholder=""
+                value={this.state.name}
+                onChange={v =>
+                  this.setState({
+                    name: v
+                  })
+                }
+                disabled={hasInfo}
+              />{' '}
+            </div>{' '}
+            <div className={styles.item}>
+              <div className={styles.label}> 手机号 </div>{' '}
+              <InputItem
+                type="phone"
+                className={styles.item__name}
+                placeholder=""
+                value={mobile}
+                error={phoneError}
+                onErrorClick={this.onPhoneErrorClick}
+                onChange={this.changePhone}
+                disabled={hasInfo}
+              />{' '}
+            </div>{' '}
+            <div className={styles.submit} onClick={this.handleSubmit}>
+              {' '}
+              {hasReport ? '查看报告' : '开始测试'}{' '}
+            </div>{' '}
+          </div>{' '}
+        </div>{' '}
+      </div>
     );
   }
 }
--- pages/question/index.js
+++ pages/question/index.js
@@ -1,168 +1,236 @@
 import React from 'react';
-import {inject, observer} from 'mobx-react';
+import { inject, observer } from 'mobx-react';
 import Link from 'next/link';
 import {
-    NavBar, List, InputItem, WhiteSpace, WingBlank, Button, Toast, Flex, Radio, Result, Icon,
-    TextareaItem, Calendar, Card, DatePickerView, DatePicker, Modal, ActivityIndicator
+  NavBar,
+  List,
+  InputItem,
+  WhiteSpace,
+  WingBlank,
+  Button,
+  Toast,
+  Flex,
+  Radio,
+  Result,
+  Icon,
+  TextareaItem,
+  Calendar,
+  Card,
+  DatePickerView,
+  DatePicker,
+  Modal,
+  ActivityIndicator
 } from 'antd-mobile';
-import {createForm} from 'rc-form';
+import { createForm } from 'rc-form';
+import classNames from 'classnames';
 import PageWrapper from '@components/PageWrapper';
-import agent from "@utils/agent";
+import agent from '@utils/agent';
 import styles from './index.less';
 import pageStore from './store';
 import storage from '@utils/storage';
-import Router from 'next/router'
-import Question from "../../constants/questions";
+import Router from 'next/router';
+import Question from '../../constants/questions';
 
 const RadioItem = Radio.RadioItem;
 
 @observer
 class Page extends React.Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
-
-        const {query} = this.props;
-        let questionIndex = 0;
-        if (query.qidx) {
-            questionIndex = Number(query.qidx);
-        }
-
-        this.state = {
-            loading: false,
-            birthday: null,
-            name: '',
-            questionIndex: questionIndex,
-            answer: null,
-            questionList: [],
-            answerMap: {}
-        };
+    const { query } = this.props;
+    let questionIndex = 0;
+    if (query.qidx) {
+      questionIndex = Number(query.qidx);
     }
 
-    async componentDidMount() {
-        try {
-            const data = await agent.Question.getList();
-            this.setState({
-                questionList: data.list
-            });
-        } catch (e) {
+    this.state = {
+      loading: false,
+      birthday: null,
+      name: '',
+      questionIndex: questionIndex,
+      answer: null,
+      questionList: [],
+      answerMap: {}
+    };
+  }
 
-        } finally {
-
-        }
+  async componentDidMount() {
+    try {
+      const data = await agent.Question.getList();
+      this.setState({
+        questionList: data.list
+      });
+    } catch (e) {
+    } finally {
     }
+  }
 
-    onChange = (value) => {
-        console.log(value);
-        this.setState({value});
-    };
+  onChange = value => {
+    console.log(value);
+    this.setState({ value });
+  };
 
-    handleSubmit = () => {
-        console.log(this.state);
-    };
+  handleSubmit = () => {
+    console.log(this.state);
+  };
 
-    handlePrev = () => {
-        const {questionIndex} = this.state;
-        if (questionIndex <= 0) {
-            return;
+  handlePrev = () => {
+    const { questionIndex } = this.state;
+    if (questionIndex <= 0) {
+      return;
+    }
+    this.setState({
+      answer: null,
+      questionIndex: questionIndex - 1
+    });
+    // Router.replace(`/question?qidx=${this.questionIndex - 1}`);
+  };
+
+  handleNext = async () => {
+    try {
+      const { questionIndex, answerMap } = this.state;
+      if (questionIndex >= Question.count() - 1) {
+        const answerList = [];
+        for (let [questionId, option] of Object.entries(answerMap)) {
+          answerList.push({
+            questionId,
+            option
+          });
         }
         this.setState({
-            answer: null,
-            questionIndex: questionIndex - 1
+          loading: true
         });
-        // Router.replace(`/question?qidx=${this.questionIndex - 1}`);
-    };
-
-    handleNext = async () => {
-        try {
-            const {questionIndex, answerMap} = this.state;
-            if (questionIndex >= (Question.count() - 1)) {
-                const answerList = [];
-                for (let [questionId, option] of Object.entries(answerMap)) {
-                    answerList.push({
-                        questionId, option
-                    });
-                }
-                this.setState({
-                    loading: true
-                });
-                const data = await agent.Question.answerList({
-                    answerList
-                });
-                const userInfo = storage.UserInfo.get();
-                Router.push('/summary?fromId=' + userInfo.userId);
-            } else {
-                this.setState({
-                    questionIndex: questionIndex + 1
-                });
-            }
-        } catch (e) {
-            console.log(e);
-        } finally {
-            this.setState({
-                loading: false
-            });
-        }
-        // console.log(this.questionIndex + 1);
-        // Router.push(`/question?qidx=${this.questionIndex + 1}`);
-    };
-
-    handleChoose = async (question, choice) => {
-        const {questionIndex, answerMap} = this.state;
-        answerMap[question.id] = choice;
+        const data = await agent.Question.answerList({
+          answerList
+        });
+        const userInfo = storage.UserInfo.get();
+        Router.push('/summary?fromId=' + userInfo.userId);
+      } else {
         this.setState({
-            answerMap,
+          questionIndex: questionIndex + 1
         });
-        setTimeout(async () => {
-            await this.handleNext();
-        }, 100);
-        // const data = await agent.Question.answer(question.id, choice);
-        // try {
-        // } catch (e) {
-        //     console.log(e);
-        // } finally {
-        //
-        // }
-    };
+      }
+    } catch (e) {
+      console.log(e);
+    } finally {
+      this.setState({
+        loading: false
+      });
+    }
+    // console.log(this.questionIndex + 1);
+    // Router.push(`/question?qidx=${this.questionIndex + 1}`);
+  };
 
-    render() {
-        const {loading, questionIndex, answerMap, questionList = []} = this.state;
-        const question = questionList[questionIndex] || {id: 0, name: ''};
-        return (
-            <div className={styles.page}>
-                {loading && (
-                    <ActivityIndicator toast text="正在加载"/>
-                )}
-                <div className={styles.number}> {questionIndex + 1} / {questionList.length}</div>
-                <div className={styles.card}>
-                    <div className={styles.card__body}>
-                        <div
-                            className={styles.question}>{questionIndex + 1}. {question.name}</div>
-                        <div onClick={this.handleChoose.bind(this, question, 'A')}
-                             className={'A' === answerMap[question.id] ? styles.answer__chosen : styles.answer}>完全能做到
-                        </div>
-                        <div onClick={this.handleChoose.bind(this, question, 'B')}
-                             className={'B' === answerMap[question.id] ? styles.answer__chosen : styles.answer}>有时能做到，有时不能做到
-                        </div>
-                        <div onClick={this.handleChoose.bind(this, question, 'C')}
-                             className={'C' === answerMap[question.id] ? styles.answer__chosen : styles.answer}>完全做不到
-                        </div>
-                        <div onClick={this.handleChoose.bind(this, question, 'D')}
-                             className={'D' === answerMap[question.id] ? styles.answer__chosen : styles.answer}>不清楚
-                        </div>
-                    </div>
-                    {/*<div className={styles.card__footer}>*/}
-                    {/*</div>*/}
-                </div>
-                <div className={styles.step}>
-                    <div onClick={this.handlePrev} className={styles.prev}>上一题</div>
-                    <div className={styles.center}></div>
-                    <div onClick={this.handleNext} className={styles.next}>下一题</div>
-                </div>
+  handleChoose = async (question, choice) => {
+    const { questionIndex, answerMap } = this.state;
+    answerMap[question.id] = choice;
+    this.setState({
+      answerMap
+    });
+    setTimeout(async () => {
+      await this.handleNext();
+    }, 100);
+    // const data = await agent.Question.answer(question.id, choice);
+    // try {
+    // } catch (e) {
+    //     console.log(e);
+    // } finally {
+    //
+    // }
+  };
+
+  render() {
+    const { loading, questionIndex, answerMap, questionList = [] } = this.state;
+    const question = questionList[questionIndex] || { id: 0, name: '' };
+    const proActive = ((questionIndex + 1) / questionList.length) * 100;
+    return (
+      <div className={styles.page}>
+        {loading && <ActivityIndicator toast text="正在加载" />}
+        <div className={styles.header}>
+          <div className={styles['progress-num']}>
+            <div className={styles.number}> {questionIndex + 1}</div>
+            <div className={styles.total}>/{questionList.length}</div>
+          </div>
+          <div className={styles.category}>身体动觉智能</div>
+        </div>
+        <div className={styles['progress-bar']}>
+          <div
+            className={styles['progress-active']}
+            style={{ width: proActive + '%' }}
+          />
+        </div>
+        {/* <div className={styles.number}> {questionIndex + 1} / {questionList.length}</div> */}
+        <div className={styles.card}>
+          <div className={styles.card__body}>
+            <div className={styles.question}>{question.name}</div>
+            <div
+              onClick={this.handleChoose.bind(this, question, 'A')}
+              className={
+                'A' === answerMap[question.id]
+                  ? styles.answer__chosen
+                  : styles.answer
+              }
+            >
+              完全能做到
             </div>
-        )
-    }
-
+            <div
+              onClick={this.handleChoose.bind(this, question, 'B')}
+              className={
+                'B' === answerMap[question.id]
+                  ? styles.answer__chosen
+                  : styles.answer
+              }
+            >
+              有时能做到，有时不能做到
+            </div>
+            <div
+              onClick={this.handleChoose.bind(this, question, 'C')}
+              className={
+                'C' === answerMap[question.id]
+                  ? styles.answer__chosen
+                  : styles.answer
+              }
+            >
+              完全做不到
+            </div>
+            <div
+              onClick={this.handleChoose.bind(this, question, 'D')}
+              className={
+                'D' === answerMap[question.id]
+                  ? styles.answer__chosen
+                  : styles.answer
+              }
+            >
+              不清楚
+            </div>
+          </div>
+          {/*<div className={styles.card__footer}>*/}
+          {/*</div>*/}
+        </div>
+        <div className={styles.step}>
+          <div className={styles['action-wp']} onClick={this.handlePrev} >
+            <img
+              src="/static/img/prev.png"
+              className={classNames(styles.icon, styles.prev)}
+            />
+            <div className={styles.text}>
+              上一题
+            </div>
+          </div>
+          <div className={styles['action-wp']} onClick={this.handleNext} >
+            <div className={styles.text}>
+              下一题
+            </div>
+            <img
+              src="/static/img/prev.png"
+              className={classNames(styles.icon, styles.next)}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default PageWrapper(createForm()(Page));
